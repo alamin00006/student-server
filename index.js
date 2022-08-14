6da92f884b7f60a3600bc6b7334d78d7385f4f84
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-08-14 18:41:35 +0600

    mongodb connent

--- index.js
+++ index.js
@@ -4,12 +4,40 @@ const port = process.env.PORT||5000;
 const cors = require('cors');
 
 
+// studentattendance
+// c2JQuWdgaoqtQFcB
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://studentattendance:c2JQuWdgaoqtQFcB@cluster0.0zpwoxc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        app.get('/user' , verifyJWT, async (req, res) =>{
+            const user = await userCollection.find().toArray();
+            console.log(user)
+            res.send(user)
+          })
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
