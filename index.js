c40a412bd4ebd12b6746ca02fb654c05e5099e5b
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-08-15 02:57:24 +0600

    all student and all teacher api create

--- index.js
+++ index.js
@@ -19,13 +19,33 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://studentattendance:c2JQuWdgaoqtQFcB@cluster0.0zpwoxc.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run() {
     try {
-        app.get('/user' , verifyJWT, async (req, res) =>{
-            const user = await userCollection.find().toArray();
-            console.log(user)
-            res.send(user)
+      await client.connect();
+      const userCollection = client.db('allUsers').collection('users');
+      const teacherCollection = client.db('allTeacher').collection('teachers');
+    
+
+        app.get('/student', async (req, res) =>{
+            const student = await userCollection.find().toArray();
+           
+            res.send(student)
           })
+
+        app.get('/teacher', async (req, res) =>{
+            const teacher = await teacherCollection.find().toArray();
+           
+            res.send(teacher)
+          })
+
+      
+    app.post('/teacher',  async(req, res) =>{
+      const teacher = req.body;
+      const result = await teacherCollection.insertOne(teacher);
+     
+      res.send(result);
+      })
 
 
     }
